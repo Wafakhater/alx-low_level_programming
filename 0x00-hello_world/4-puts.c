5493c15567c7b2be48f851edf22a4bdff7aa484d
Wafakhater <wafakhatir558@gmail.com>
2023-08-10 06:50:15 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,1 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: A C program that prints with put function
- *
- * Return: Alawys 0 (Success)
-*/
-
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (O);
-}
+#include <stdio.h>  /**  * main - Entry point  *  * Description: A C program that prints with put function  *  * Return: Alawys 0 (Success) */  int main(void) { puts("\"Programming is like building a multilingual puzzle"); return (O); }
