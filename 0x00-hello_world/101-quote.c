ba1b910a71caf600e240f59c64321d96507b007b
Wafakhater <wafakhatir558@gmail.com>
2023-08-10 09:02:01 -0700

    done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- *
- *  Description: print a qoute using write function
- *  ssize_t write(int fd.const void *buf,size_t count);
- *
- *  Return: 1 (error).
+ * main - A c program that prints a line to the standard error
+ *  Return: 1 (Success)
  */
 int main(void)
 {
-char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1. quo .59);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
