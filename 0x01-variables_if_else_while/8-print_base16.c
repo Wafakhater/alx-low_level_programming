472ea5bcb9f91ef96a7546a9b97061bddeaeb114
Wafakhater <wafakhatir558@gmail.com>
2023-08-11 10:31:10 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@ int main(void)
 {
 int digit 48; /*48; decimal rep of 0*/
 while (digit <= 102) /*102; decimal rep of f*/
-{
 putchar(digit);
-/*after 9 we jump till 96: */
+{
+/*after 9 we jump till 96; */
 if (digit == 57)
 digit += 39;
 digit++;
