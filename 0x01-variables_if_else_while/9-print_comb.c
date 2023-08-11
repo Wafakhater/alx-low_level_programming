df90e60685f30a1aa9dd249a3b992491cbad3a21
Wafakhater <wafakhatir558@gmail.com>
2023-08-11 11:01:29 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - enrty point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+putchar(digit + 48);
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+digit++;
+}
+putchar('\n');
+return (0);
+}
