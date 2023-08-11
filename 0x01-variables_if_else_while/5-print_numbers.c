41be584f1e9741e9413d4cae265bbee879e72407
Wafakhater <wafakhatir558@gmail.com>
2023-08-11 09:43:34 -0700

    done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - enrty point
+ *
+ * Description: print all digit numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%i", digit);
+digit++;
+}
+printf("\n");
+return (0);
+}
