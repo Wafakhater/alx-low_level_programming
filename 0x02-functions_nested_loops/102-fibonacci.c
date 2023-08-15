fd6b919a692a82909cc660d6d343eb79059ef3c9
Wafakhater <wafakhatir558@gmail.com>
2023-08-15 10:35:03 -0700

    done

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,12 +5,12 @@
  * Description: prints the first 50 Fibonacci numbers starting
  * with 1 and 2 folled by a new line
  *
- * Return: 0 (success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
 int count;
-unsigned long fib1 = 0, fibs = 1, sum;
+unsigned long fib1 = 0, fib2 = 1, sum;
 for (count = 0; count < 50; count++)
 {
 sum = fib1 + fib2;
