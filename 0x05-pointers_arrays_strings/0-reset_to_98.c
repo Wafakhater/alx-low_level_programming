9146f08fa8aa706c7a348b97d3d56ace5a442b99
Wafakhater <wafakhatir558@gmail.com>
2023-08-21 02:55:57 -0700

    done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer to  int parameter and
+ * updates the value is points to 98
+ * @n: int parameter
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
