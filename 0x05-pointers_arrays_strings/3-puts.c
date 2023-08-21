96fe257f1ec4a0d9ca160273ab2858a3241fa300
Wafakhater <wafakhatir558@gmail.com>
2023-08-21 03:30:27 -0700

    done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _puts - print a string, followed by a newline to stdont
+ * _puts - print a string, followed by a newline
  * @str: string parameter input
  * Return: length of a string
  */
-void _puts(char *str);
+void _puts(char *str)
 {
 while (*str != '\0')
 {
-_putchar(*str + 0);
-*str++;
+putchar(*str + 0);
+str++;
 }
-_putchar('\n');
+putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void _puts(char *str);
+#endif /*MAIN_H */
