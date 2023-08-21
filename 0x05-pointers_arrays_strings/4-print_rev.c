d2941e03150b379f05868af51a904062a8e229e3
Wafakhater <wafakhatir558@gmail.com>
2023-08-21 03:37:40 -0700

    done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints revarsed string, followed by a newline
+ * @s: pointer to the string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
