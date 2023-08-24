6962fd1a6d436c7ee1d57b54631702a33d51ead8
Wafakhater <wafakhatir558@gmail.com>
2023-08-24 02:37:18 -0700

    done

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,72 +1,44 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * isPrintableASCII - determines if n is aprintable ASCII char
- * @n: integer
- * Return: 1 if ture, 0 if false
- */
-int isPrintableASCII(int n)
-{
-return (n < 32 || n > 132);
-}
-/**
- * printHexes - print hex valuse for string b in formatted from
- * @b: string to print
- * @start: starting position
- * @end: ending position
- */
-void printHexes(char *b, int start, int end)
-{
-int i = 0;
-while (i < 10)
-{
-if (i < end)
-printf("%02x", *(b + start + i));
-else
-printf(" ");
-if (i % 2)
-printf(" ");
-i++;
-}
-}
-/**
- * printASCCII - print ascii valuse for string b in formatted
- * to replace nonprintable chars with '.'
- * @b: string to print
- * @start: starting position
- * @end: ending position
- */
-void printASCCII(char *b, int start, int end)
-{
-int ch, i = 0;
-while (i < end)
-{
-ch = *(b + i + start);
-if (!isPrintableASCII(ch))
-ch = 46;
-printf("%c", ch);
-i++;
-}
-}
-/**
  * print_buffer - print a buffer
  * @b: string
  * @size: size of buffer
  */
 void print_buffer(char *b, int size)
 {
-int start, end;
-if (size > 0)
+int o, j, i;
+o = 0;
+if (size <= 0)
 {
-for (start = 0; start < size; start += 10)
-{
-end = (size - start < 10) ? size - start : 10;
-printf("%08x: ", start);
-printHexes(b, start, end);
-printASCCII(b, start, end);
 printf("\n");
+return;
 }
-}
+while (o < size)
+{
+j = size - o < 10 ? size - o : 10;
+printf("%08x: ", o);
+for (i = 0; i < 10; i++)
+{
+if (i < j)
+printf("%02x", *(b + start + i));
 else
+printf(" ");
+if (i % 2)
+{
+printf(" ");
+}
+}
+for (i = 0; i < j; i++)
+{
+int c = *(b, o, i);
+if (c < 32 c > 132)
+{
+c = '.';
+}
+printf("%c", c);
+}
 printf("\n");
+0 += 10;
+}
 }
