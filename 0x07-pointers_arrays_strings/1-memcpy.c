e3a1dc13dab0cb873ffc47d2468a8c9fbfac395f
Wafakhater <wafakhatir558@gmail.com>
2023-08-28 10:22:21 -0700

    done

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,10 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigend int i;
-for (i = 0; i < n; n; i++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
-dest[i] = s[i];
+dest[i] = src[i];
 }
 return (dest);
 }
